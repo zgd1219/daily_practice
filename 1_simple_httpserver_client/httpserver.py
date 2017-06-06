6883bc13b0a2474d951598bfe4789f2be14f6e66
Zhou Guangda <anotherzgd@sina.cn>
2017-06-06 10:57:16 +0800

    简单的httpserver_client
    hello world，最简单的socket套路

---
+++ 1_simple_httpserver_client/httpserver.py
@@ -0,0 +1,26 @@
+import socket
+
+
+host = '127.0.0.1'
+port = 2000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+print('服务器监启动，端口为:{}...'.format(port))
+s.listen(5)
+print('服务器监听已启动...')
+
+# socket服务器端固定套路，socket.socket --> bind --> listen --> accept
+
+while True:
+    connection, addr = s.accept()
+    print('客户端{}已连接'.format(addr))
+
+
+    raw_request = connection.recv(1024)
+    request = raw_request.decode('utf-8')
+    print('ip and request, {}\n{}'.format(addr, request))
+    connection.sendall('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
+                       '<h3>Hello world!</h3>'.encode('utf-8'))
+    connection.close()
+
+
---
+++ 1_simple_httpserver_client/simpleclient.py
@@ -0,0 +1,24 @@
+import socket
+
+
+host = '127.0.0.1'
+port = 2000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+# socket客户端, socket.socket --> connect
+
+def get_response(s):
+    response = b''
+    while True:
+        r = s.recv(1024)
+        if len(r) == 0:
+            break
+        response += r
+    return response
+
+request = 'GET / HTTP/1.1\r\nHost: {}\r\n\r\n'.format(host)
+s.send(request.encode('utf-8'))
+response = get_response(s)
+print('response响应', response.decode('utf-8'))
+
